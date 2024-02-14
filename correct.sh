fc2e8443789c3577354b5c61d84c2c8589b74e1c
ttuff <ty.tuff@colorado.edu>
2024-02-14 23:38:02 +0000

    bash script edits

--- correct.sh
+++ correct.sh
@@ -5,18 +5,16 @@ echo "$1"
 
 filename="${1%.h5}"
 
-echo "$filename"
-
 python neon2envi2.py $1 output -anc
 
 python config_generator.py
 
 python image_correct.py output/config_0.json
 
-output="${filename}__after_correction.tif"
+output_after="${filename}__after_correction.tif"
 
-gdal_translate -of GTiff export/ENVI__corrected_0 output
+gdal_translate -of GTiff export/ENVI__corrected_0 $output_after
 
-output="${filename}__before_correction.tif"
+output_before="${filename}__before_correction.tif"
 
-gdal_translate -of GTiff output/ENVI output
+gdal_translate -of GTiff output/ENVI $output_before
