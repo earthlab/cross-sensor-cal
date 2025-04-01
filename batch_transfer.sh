874ea129eecc347d1793d5f4a2e6ad860c568b6c
ttuff <ty.tuff@colorado.edu>
2025-04-01 16:23:50 +0000

    trying to get paths right

--- batch_transfer.sh
+++ batch_transfer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Path to the CSV move list
-CSV_FILE="cross-sensor-cal/sorted_files/file_move_list.csv"
+CSV_FILE="home/shared/earthlab/macrosystems/cross-sensor-cal/sorted_files/envi_file_move_list.csv"
 
 # Read the CSV file and execute gocmd for each line
 tail -n +2 "$CSV_FILE" | while IFS=, read -r source_path destination_path; do
-    # Construct the absolute iRODS destination path
-    irods_destination="/iplant/home/shared/earthlab/macrosystems/$destination_path"
+    # Force absolute iRODS path with /iplant prefix
+    irods_destination="/iplant/$destination_path"
     echo "Transferring: $source_path -> $irods_destination"
     ./gocmd put --diff --icat --retry 3 -d -k "$source_path" "$irods_destination"
 done
